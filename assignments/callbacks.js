b8e10d3f15c28fccb5031c7bd87531bd7fa8674c
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-01-18 19:05:15 +0800

    Complete callback MVP questions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,27 +21,91 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// Question #1
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
 
+// Solution Version 1
+function getLength(arr, cb) {
+  return cb(arr.length)
+}
+
+getLength(items, function(length) {
+  console.log(length)
+})
+
+// Solution Version 2
+function displayLength(arr) {
+  console.log(arr.length)
+}
+
+function getLength(arr, cb) {
+  return cb(arr)
+}
+
+getLength(items, displayLength)
+
+
+
+// Question #2
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
+// Solution
+function last(arr, cb) {
+  return cb(arr[arr.length-1])
+}
+
+last(items, function (item) {
+  console.log(item)
+})
+
+// Question #3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
+// Solution
+function sumNums(x, y, cb) {
+  const sum = x + y
+  return cb(sum)
+}
+
+sumNums(164, 320, function(answer) {
+  console.log(answer)
+})
+
+// Question #4
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
+// Solution
+function multiplyNums(x, y, cb) {
+  const product = x * y
+  return cb(product)
+}
+
+multiplyNums(324, 875, function (answer) {
+  console.log(answer)
+})
+
+// Question #5
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+// Solution
+function contains(item, list, cb) {
+  return cb(list.includes(item))
+}
+
+contains('yo-yo', items, function(item) {
+  console.log(item)
+})
 
 /* STRETCH PROBLEM */
 
