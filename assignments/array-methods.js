7a84cf2e1db3941d2bbe591344862ecd379e8997
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-01-19 04:29:42 +0800

    Complete the remaining array-method challenges

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -80,22 +80,42 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-// runners.filter(function(runner) {
-//   return 
-// })
+let largeShirts = runners.filter(function(runner) {
+  return runner.shirt_size === "L"
+})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+// Solution 
+let ticketPriceTotal = runners.map(function(runner) {return runner.donation})
+                              .reduce(function(total, num) {return total + num})
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 - Find the average donation amount for this year's race?
+const averageDonation = runners.map(function (runner) { return runner.donation })
+  .reduce(function (total, num) { return (total + num) }) / runners.length
 
-// Problem 2
+console.log(averageDonation)
 
-// Problem 3
+// Problem 2 - How many companies were represented in this race?
+let companies = runners.map(function (runner) { return runner.company_name })
+let distinctCompanies = [...new Set(companies)]
+
+console.log(distinctCompanies.length)
+
+// Problem 3 - To prevent the large t-shirt size debacle from happening again this year, 
+//the marketing team wants forecase orders for next year's race and 
+//needs a breakdown of how many T-shirts were requested based on shirt size
+let sizes = runners.map(function (runner) { return runner.shirt_size })
+let sizeCategories = [...new Set(sizes)]
+console.log(sizeCategories)
+
+for (let i = 0; i < sizeCategories.length; i++) {
+  let size = runners.filter(function(runner) {return runner.shirt_size === sizeCategories[i]}) //closure example
+  console.log(`There were ${size.length} orders for ${sizeCategories[i]} size.`)
+}
